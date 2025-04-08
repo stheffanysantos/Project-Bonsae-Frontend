73a22ccf5f916711251c2ef6202f475e6fc25a7f
vinicius-pascoal <112434231+vinicius-pascoal@users.noreply.github.com>
2025-04-07 23:25:43 -0300

    init xlsx reader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold mb-4">Visualizador de Planilha</h1>
+
+    <input type="file" @change="handleFileUpload" accept=".xlsx" class="mb-4" />
+
+    <div v-if="paginatedData.length" class="overflow-auto max-h-[500px]">
+      <table class="table-auto w-full border border-gray-300">
+        <thead class="bg-gray-200">
+          <tr>
+            <th v-for="(header, index) in headers" :key="index" class="border px-4 py-2">
+              {{ header }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, rowIndex) in paginatedData" :key="rowIndex">
+            <td v-for="(header, index) in headers" :key="index" class="border px-4 py-2">
+              {{ row[header] }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="mt-4 flex gap-2">
+      <button @click="currentPage--" :disabled="currentPage === 1"
+        class="px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-50">
+        Anterior
+      </button>
+
+      <span>Página {{ currentPage }} de {{ totalPages }}</span>
+
+      <button @click="currentPage++" :disabled="currentPage === totalPages"
+        class="px-3 py-1 bg-blue-500 text-white rounded disabled:opacity-50">
+        Próxima
+      </button>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+import * as XLSX from "xlsx"
+import { ref, computed } from "vue"
+
+const data = ref([])
+const headers = ref([])
+const currentPage = ref(1)
+const itemsPerPage = 10
+
+const handleFileUpload = (event) => {
+  const file = event.target.files[0]
+  if (!file) return
+
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    const dataBinary = new Uint8Array(e.target.result)
+    const workbook = XLSX.read(dataBinary, { type: "array" })
+
+    const sheetName = workbook.SheetNames[0]
+    const sheet = workbook.Sheets[sheetName]
+    const jsonData = XLSX.utils.sheet_to_json(sheet)
+
+    data.value = jsonData
+    headers.value = Object.keys(jsonData[0] || {})
+    currentPage.value = 1
   }
+  reader.readAsArrayBuffer(file)
 }
+
+const totalPages = computed(() =>
+  Math.ceil(data.value.length / itemsPerPage)
+)
+
+const paginatedData = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage
+  return data.value.slice(start, start + itemsPerPage)
+})
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+table {
+  border-collapse: collapse;
 }
 </style>

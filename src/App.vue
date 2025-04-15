5d4a787ea76f75c5a1fba3a698d084444c3d7a78
Vinicius <viniciuspascoal013@gmail.com>
2025-04-15 08:49:23 -0300

    init rotas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <CsvUploader />
-
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-import CsvUploader from './components/CsvUploader.vue';
 export default {
-  name: 'App',
-  components: {
-    CsvUploader
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+nav {
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/views/DisciplinasView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>funcionou</h1>
+  </div>
+</template>

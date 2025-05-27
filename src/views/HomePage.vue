22cb73ccbf2231d35bb338640dbb6292c3ad560a
Vinicius <viniciuspascoal013@gmail.com>
2025-05-27 08:21:08 -0300

    fundo novo e nova tela inicial

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,33 +1,14 @@
 <template>
-  <div class="fundo-principal">
-    <main>
-      <div class="containerHome">
-        <div class="content-text">
-          <h1>
-            Bem Vindo ao ambiente de <br />
-            <span class="highlight">importação CSV</span> !
-          </h1>
-          <p>
-            Importe seus arquivos CSV de forma rápida, segura e sem complicações. <br />
-          </p>
-
-        </div>
-
-        <div class="contentImgHome">
-          <div class="carousel">
-            <div class="carousel-item">
-              <img src="../assets/img/imagemTelaInicial.png" alt="imagem inicial" />
-            </div>
-          </div>
-        </div>
+  <div class="bodyScreen">
+    <div class="containerHome">
+      <div>
+        <h1>
+          Bem Vindo ao ambiente de <br />
+          <span class="gradient-text">importação CSV</span>
+        </h1>
       </div>
-    </main>
-
-    <footer>
-      <form class="btnsHome" @submit.prevent="handleLogin">
-        <button @click="goProcessos" class="btn-principal">Entrar</button>
-      </form>
-    </footer>
+      <button @click="goProcessos" class="btn-principal">Entrar</button>
+    </div>
   </div>
 </template>
 

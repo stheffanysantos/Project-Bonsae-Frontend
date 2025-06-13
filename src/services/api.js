8bd3777ebfd620dc5329ec202187de1b28790199
Seu Nome <seu@email.com>
2025-06-12 23:45:14 -0300

    agora vaaaaaiii

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://bonsaeb-production.up.railway.app/',
+    baseURL: 'https://dae2-2804-14c-4384-8657-00-1045.ngrok-free.app',
     timeout: 5000,
 })
 
